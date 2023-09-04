2330d71cf0a439f9560f0e53daf778316e16dafb
LTianRahul <kkrh00@gmail.com>
2023-09-04 20:35:26 +0530

    Updating the Behave Python with latest selenium

--- features/environment.py
+++ features/environment.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 import os
 import json
+from selenium.webdriver.firefox.options import Options
+import subprocess
+
 
 INDEX = int(os.environ['INDEX']) if 'INDEX' in os.environ else 0
 if os.environ.get("env") == "jenkins":
@@ -14,23 +17,62 @@ else:
 username = os.environ["LT_USERNAME"]
 authkey = os.environ["LT_ACCESS_KEY"]
 
-
 def before_feature(context, feature):
-    desired_cap = setup_desired_cap(CONFIG[INDEX])
-    context.browser = webdriver.Remote(
-        desired_capabilities=desired_cap,
-        command_executor="https://%s:%s@hub.lambdatest.com:443/wd/hub" % (username, authkey)
-    )
+        options =  Options()
+        options.browser_version = "114.0"
+        options.platform_name = "Windows 10"
+        lt_options = {};
+        lt_options["username"] = "";
+        lt_options["accessKey"] = "";
+        lt_options["browserName"] = "Chrome";
+        lt_options["browserVersion"] = "latest";
+        # lt_options["tunnel"] = True;
+        lt_options["build"] = "TestingSampleBehavePython";
+        lt_options["w3c"] = True;
+        lt_options["plugin"] = "python-python";
+        lt_options["selenium_version"] = "4.10.0";
+        options.set_capability('LT:Options', lt_options);
+        # "browserName": "Chrome",
+		# "browserVersion": "latest"
+        # Steps to run Smart UI project (https://beta-smartui.lambdatest.com/)
+        # Step - 1 : Change the hub URL to @beta-smartui-hub.lambdatest.com/wd/hub
+        # Step - 2 : Add "smartUI.project": "<Project Name>" as a capability above
+        # Step - 3 : Run "driver.execute_script("smartui.takeScreenshot")" command wherever you need to take a screenshot
+        # Note: for additional capabilities navigate to https://www.lambdatest.com/support/docs/test-settings-options/
+        context.driver = webdriver.Remote(
+            command_executor="http://rahulkumarlambdatest:dboZK7so8koMnIR1tN11aKfMgxyKtDpb90IlyaCj4n6n7tQeK6@hub.lambdatest.com/wd/hub".format(
+                lt_options["username"], lt_options["accessKey"]),
+            options=options)
+ 
+
+# def before_feature(context, feature):
+#     desired_cap = setup_desired_cap(CONFIG[INDEX])
+#     context.browser = webdriver.Remote(
+#         desired_capabilities=desired_cap,
+#         command_executor="https://%s:%s@hub.lambdatest.com:443/wd/hub" % (username, authkey)
+#     )
 
 
 def after_feature(context, feature):
-    context.browser.quit()
+    context.driver.quit()
+
+# if __name__ == "__main__":
+
+# 	#start tunnel process
+# 	tunnel_process = subprocess.Popen(["./LT","--user",username,"--key",authkey],stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT)
+	
+# 	# #run testcases
+# 	# unittest.main()
+
+# 	#end tunnel
+# 	tunnel_process.terminate()
 
 
-def setup_desired_cap(desired_cap):
-    """
-    sets the capability according to LT
-    :param desired_cap:
-    :return:
-    """
-    return desired_cap
+     
+# def setup_desired_cap(desired_cap):
+#     """
+#     sets the capability according to LT
+#     :param desired_cap:
+#     :return:
+#     """
+#     return desired_cap
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -2,34 +2,35 @@
 Selenium steps to configure behave test scenarios
 """
 import time
+from selenium.webdriver.common.by import By
 
 @when('visit url "{url}"')
 def step(context, url):
-    context.browser.get(url)
+    context.driver.get(url)
 
 
 @when('check if title is "{title}"')
 def step(context, title):
-    assert context.browser.title == title
+    assert context.driver.title == title
 
 
 @when('field with name "First Item" is present check the box')
 def step(context):
-    context.browser.find_element_by_name("li1").click()
-
+    Element_ID1 = context.driver.find_element(By.NAME, "li1").click()
+   
 
 @when('field with name "Second Item" is present check the box')
 def step(context):
-    context.browser.find_element_by_name("li3").click()
+    context.driver.find_element(By.NAME, 'li3').click()
 
 
 @when('select the textbox add "{text}" in the box')
 def step(context, text):
-    context.browser.find_element_by_id("sampletodotext").click()
-    context.browser.find_element_by_id("sampletodotext").clear()
-    context.browser.find_element_by_id("sampletodotext").send_keys(text)
+    context.driver.find_element(By.ID, "sampletodotext").click()
+    context.driver.find_element(By.ID, "sampletodotext").clear()
+    context.driver.find_element(By.ID, "sampletodotext").send_keys(text)
 
 
 @then('click the "{button}"')
 def step(context, button):
-    context.browser.find_element_by_id(button).click()
+    context.driver.find_element(By.ID, "addbutton").click()
